8968ef06aade0b37dfcf46fdd239c38259aa6fbb
Alex Jeffreys <alex.jeffreys@red-gate.com>
2022-01-21 16:18:07 +0000

    Clean up formatting

--- WordleSolver/Program.fs
+++ WordleSolver/Program.fs
@@ -1,5 +1,5 @@
-open System
 open System.IO
+
 let wordList = File.ReadLines("WordList.txt")
 
 type Word = Word of string
@@ -8,6 +8,7 @@ type LetterClue =
     | MatchCorrectPosition
     | MatchWrongPosition
     | NoMatch
+
 type Clue = Clue of LetterClue list
 
 let prettyPrint (Clue letterClues) =
@@ -19,25 +20,23 @@ let prettyPrint (Clue letterClues) =
         | NoMatch -> "R")
     |> String.concat ""
 
-let pairUp (string1:string) (string2:string) =
+let pairUp (string1: string) (string2: string) =
 
     let rec pairUpLists list1 list2 =
         match list1, list2 with
         | [], [] -> []
-        | head1::tail1, head2::tail2 -> (head1, head2)::(pairUpLists tail1 tail2)
+        | head1 :: tail1, head2 :: tail2 -> (head1, head2) :: (pairUpLists tail1 tail2)
         | _ -> invalidOp "Lists have mismatching number of elements"
 
     pairUpLists (string1 |> Seq.toList) (string2 |> Seq.toList)
 
 let matchPattern (Word guess) (Word actual) =
     let pairedLetters = pairUp guess actual
+
     pairedLetters
     |> List.map
         (function
-         | guessLetter, actualLetter when guessLetter = actualLetter -> MatchCorrectPosition
-         | guessLetter, _ when actual.Contains(guessLetter) -> MatchWrongPosition
-         | _ -> NoMatch
-            )
+        | guessLetter, actualLetter when guessLetter = actualLetter -> MatchCorrectPosition
+        | guessLetter, _ when actual.Contains(guessLetter) -> MatchWrongPosition
+        | _ -> NoMatch)
     |> Clue
-
-
