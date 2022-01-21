0d93ae6b981493ee5531f874a5b790d282b112a8
Alex Jeffreys <alex.jeffreys@red-gate.com>
2022-01-21 16:00:34 +0000

    Add code for comparing words

--- WordleSolver/Program.fs
+++ WordleSolver/Program.fs
@@ -1,4 +1,29 @@
+open System
+open System.IO
+let wordList = File.ReadLines("WordList.txt")
+
+type Word = Word of string
+type Clue = Clue of string
+
+let pairUp (string1:string) (string2:string) =
+
+    let rec pairUpLists list1 list2 =
+        match list1, list2 with
+        | [], [] -> []
+        | head1::tail1, head2::tail2 -> (head1, head2)::(pairUpLists tail1 tail2)
+        | _ -> invalidOp "Lists have mismatching number of elements"
+
+    pairUpLists (string1 |> Seq.toList) (string2 |> Seq.toList)
+
+let matchPattern (Word guess) (Word actual) =
+    let pairedLetters = pairUp guess actual
+    pairedLetters
+    |> List.map (
+        fun (guessLetter, actualLetter) ->
+        if guessLetter = actualLetter then "g"
+        elif actual.Contains(guessLetter) then "y"
+        else "r"
+        )
+    |> String.concat ""
 
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
